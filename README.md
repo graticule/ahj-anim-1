fd99b288a265dc5aecfa12ffaf28da327ac314b1
Daniil Kozlov <daniil.a.kozlov@gmail.com>
2023-10-28 00:18:39 +0800

    fix lint and no tests problem

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-![CI](https://github.com/graticule/ahj-anim/actions/workflows/web.yml/badge.svg)
+![CI](https://github.com/graticule/ahj-anim-1/actions/workflows/web.yml/badge.svg)
 
-https://graticule.github.io/ahj-anim/
+https://graticule.github.io/ahj-anim-1/
--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,5 @@ import Collapsable from "./collapsable/collapsable";
 
 document.addEventListener("DOMContentLoaded", () => {
   const collapsableElements = document.querySelectorAll(".collapsable");
-  const collapsables = [...collapsableElements].map(el => new Collapsable(el))
+  [...collapsableElements].map((el) => new Collapsable(el));
 });
